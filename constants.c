ad94596d7702f0e6e3635528015cb0b571b0bed7
Tiago Silva <tbrancosilva@hotmail.com>
2021-12-13 17:42:48 +0000

    ➕ added constants

---
+++ constants.c
@@ -0,0 +1,5 @@
+//
+// Created by James Whitever on 13/12/2021.
+//
+
+#include "constants.h"
---
+++ constants.h
@@ -0,0 +1,10 @@
+//
+// Created by James Whitever on 13/12/2021.
+//
+
+#ifndef PROJECT_CONSTANTS_H
+#define PROJECT_CONSTANTS_H
+
+static const char COLORS[] = {'R', 'G', 'B', 'W', 'Y', 'O'};
+
+#endif //PROJECT_CONSTANTS_H
