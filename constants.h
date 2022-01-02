2b4fb737a74b51c425f0221573bf53cead68a21d
Tiago Silva <tbrancosilva@hotmail.com>
2022-01-02 18:44:47 +0000

    ➕ Added terminal colors

--- constants.h
+++ constants.h
@@ -9,4 +9,20 @@ static const char COLORS[] = {'R', 'G', 'B', 'W', 'Y', 'O'};
 
 static const int COLORS_LENGTH = sizeof COLORS / sizeof(char);
 
+static const char TERMINAL_COLOR_DEFAULT[] = "\x1B[0m";
+
+static const char TERMINAL_COLOR_RED[] = "\x1B[31m";
+
+static const char TERMINAL_COLOR_GREEN[] = "\x1B[32m";
+
+static const char TERMINAL_COLOR_YELLOW[] = "\x1B[33m";
+
+static const char TERMINAL_COLOR_BLUE[] = "\x1B[34m";
+
+static const char TERMINAL_COLOR_MAGENTA[] = "\x1B[35m";
+
+static const char TERMINAL_COLOR_CYAN[] = "\x1B[36m";
+
+static const char TERMINAL_COLOR_WHITE[] = "\x1B[37m";
+
 #endif //PROJECT_CONSTANTS_H
